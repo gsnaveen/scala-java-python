936f7f38500fd73c92b34dd5dd65ee10d28e394f
Naveen <gsnaveen@yahoo.com>
2018-08-27 17:23:00 -0700

    Update downloadFiles_sftp.py

--- downloadFiles_sftp.py
+++ downloadFiles_sftp.py
@@ -9,3 +9,8 @@ with pysftp.Connection('download.myweb.com', username='myusername', password='my
             if re.search("data_"+ fileDate +".*\.csv\.gz", filename):
                 print(filename)
                 sftp.get(filename, "./data/"+ filename, preserve_mtime=True)
+                
+try:
+    call(["gunzip", "*" + fileDate + "*"]) 
+except:
+    print("Error while Unzipping")
