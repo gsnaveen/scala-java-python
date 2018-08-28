304745045acbd36573358a9fd308441ccdf5f5a2
Naveen <gsnaveen@yahoo.com>
2018-08-27 17:12:00 -0700

    Create downloadFiles_sftp.py

---
+++ downloadFiles_sftp.py
@@ -0,0 +1,11 @@
+import pysftp
+import re
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+fileDate = "20180826"
+with pysftp.Connection('download.myweb.com', username='myusername', password='mypassword', cnopts=cnopts) as sftp:
+    with sftp.cd('mastersurge'):             # temporarily chdir to public
+        for filename in sftp.listdir():
+            if re.search("data_"+ fileDate +".*\.csv\.gz", filename):
+                print(filename)
+                sftp.get(filename, "./data/"+ filename, preserve_mtime=True)
