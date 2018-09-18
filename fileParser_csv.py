39d00307751c2aa5ac4517d7da6b5f295c00e805
Naveen <gsnaveen@yahoo.com>
2018-09-17 19:06:30 -0700

    Create fileParser_csv.py

---
+++ fileParser_csv.py
@@ -0,0 +1,27 @@
+def parse(line):
+    output = []
+    mystring = ""
+    ActiveQuote = False
+    output.append("START")
+    for char in line:
+        if ActiveQuote == False and char == "'":
+            ActiveQuote = True
+        elif ActiveQuote == True and char == "'":
+            ActiveQuote = False
+        elif char == ',' and ActiveQuote != True :
+            output.append(mystring)
+            mystring = ""
+        else:
+             mystring += char
+    else:
+        output.append(mystring)
+        output.append("END")
+        
+    return output
+
+myString = "Hello,World"
+myString = "'this',is,'good,bad'"
+
+
+parse(myString)
+
