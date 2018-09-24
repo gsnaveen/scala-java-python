b67a48349415142e97b0b894e40a3314dca94aa2
Naveen <gsnaveen@yahoo.com>
2018-09-24 07:58:55 -0700

    Update datasets_merging.py

--- datasets_merging.py
+++ datasets_merging.py
@@ -2,23 +2,25 @@ import os
 import pandas as pd
 
 basePath = "./mlData"
+baseOutPath = "./mlOutData"
 joinKey = ["customer"]
 inData = os.listdir(basePath)
 totalFiles = len(inData)
 
 if totalFiles < 2:
-	print("Not enough Files to merge")
-	return
+    print("Not enough Files to merge")
+    exit
 
 toProcessFile = inData[0]
 mainDataSet = pd.read_csv(basePath+"/"+toProcessFile,sep="\t",header='infer')
 
 for i in range(1,totalFiles):
-	toProcessFile = inData[i]
-	nexttoProcessFile = pd.read_csv(basePath+"/"+toProcessFile,sep="\t",header='infer')
-	mainDataSet = pd.merge(mainDataSet, nexttoProcessFile, on=joinKey,how='outer')
+    toProcessFile = inData[i]
+    nexttoProcessFile = pd.read_csv(basePath+"/"+toProcessFile,sep="\t",header='infer')
+    mainDataSet = pd.merge(mainDataSet, nexttoProcessFile, on=joinKey,how='outer')
 mainDataSet = mainDataSet.fillna(0)
-mainDataSet
+
+mainDataSet.to_csv(baseOutPath+"/"+ "_".join(joinKey) +".txt",sep='\t',header=True, index=False)
 
 '''
 customer 	clicks 	downloads 	visits
