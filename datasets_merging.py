79042d571bde5e817daf04a730eef211964a0aca
Naveen <gsnaveen@yahoo.com>
2018-09-24 07:48:37 -0700

    Update datasets_merging.py

--- datasets_merging.py
+++ datasets_merging.py
@@ -1,23 +1,22 @@
 import os
 import pandas as pd
 
-basePath = "./mlData" # Input directory of the datasets
-joinKey = ["customer"] # Common keys for the datasets. to be joined on
-
+basePath = "./mlData"
+joinKey = ["customer"]
 inData = os.listdir(basePath)
 totalFiles = len(inData)
 
 if totalFiles < 2:
-    print("Not enough Files to merge")
-    return
+	print("Not enough Files to merge")
+	return
 
 toProcessFile = inData[0]
 mainDataSet = pd.read_csv(basePath+"/"+toProcessFile,sep="\t",header='infer')
 
 for i in range(1,totalFiles):
-    toProcessFile = inData[i]
-    nexttoProcessFile = pd.read_csv(basePath+"/"+toProcessFile,sep="\t",header='infer')
-    mainDataSet = pd.merge(mainDataSet, nexttoProcessFile, on=joinKey,how='outer')
+	toProcessFile = inData[i]
+	nexttoProcessFile = pd.read_csv(basePath+"/"+toProcessFile,sep="\t",header='infer')
+	mainDataSet = pd.merge(mainDataSet, nexttoProcessFile, on=joinKey,how='outer')
 mainDataSet = mainDataSet.fillna(0)
 mainDataSet
 
