273eb1e0c9bcfa8cb9d029ad4772301135a92550
Naveen <gsnaveen@yahoo.com>
2019-02-17 09:47:20 -0800

    Update exportingToPDF.py

--- exportingToPDF.py
+++ exportingToPDF.py
@@ -1,19 +1,51 @@
-#https://wkhtmltopdf.org/downloads.html
-# install and add binary/bin folder to the path
-import pandas as pd
-import pdfkit as pdf
-from jinja2 import Template,FileSystemLoader,Environment
-import re,sys,os,json
 
+import pdfkit as pdf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from jinja2 import Template,FileSystemLoader,Environment
+import os
+basePathDestinationFolder = "C:/Users/Box/users/"
+jinja_env = Environment(loader=FileSystemLoader('./template/'), trim_blocks=True, lstrip_blocks=True)
 inputHTMLtemplate =  "inputHTMLFile"
-context = {'pic1': './images/pic_trulli'}
-jinja_env = Environment(loader=FileSystemLoader('./template/'),trim_blocks=True, lstrip_blocks=True)
-template = jinja_env.get_template(inputHTMLtemplate+'.html.j2')
-output = template.render(context)
-with open(inputHTMLtemplate+".html","+w" ) as fp:
-    fp.write(output)
-# df = pd.DataFrame.from_records([[1,11,111],[2,22,222]],columns=["fist","second","third"])
-# df.to_html('./data/mydf.html')
-outPdffile='./data/mydfhtml2.pdf'
-pdf.from_file('./data/my.html', outPdffile)
-# pdf.from_file(inputHTMLtemplate+ '.html', outPdffile)
+template = jinja_env.get_template(inputHTMLtemplate + '.html.j2')
+
+df = pd.read_csv("./data/inputData.csv",sep=",",header=0)
+df["sav_id_s"] = df["sav_id_s"].astype(str)
+
+
+for email in df["email"].unique():
+    dfPlot = df[df["email"] == email].reset_index()
+    fullName = dfPlot['full_name'].iloc[0]
+    # print(fullName)
+    dfPlot.index = np.arange(1, len(dfPlot) + 1)
+
+    savs = list(dfPlot["sav_id_s"])
+    thescore = list(dfPlot["theScore"])
+    plt.plot(savs, thescore, color='g')
+    # plt.xticks(savs, rotation='vertical')
+    plt.xticks(rotation=75)
+    # plt.tight_layout()
+    plt.gcf().subplots_adjust(bottom=0.20)
+    plt.xlabel('SavIDs')
+    plt.ylabel('Surge Score')
+    plt.title('Surge Score Chart for ' + fullName)
+    # plt.show()
+    plt.savefig("./images/"+ email+'.jpg')
+    dfPlot[["id","Node_Level_3","Node_Level_4","theScore"]].to_html("./datadf/"+ email + ".html") #,index=False
+    plt.close()
+
+    # Working on the template and pdf
+    context = {'pic1': '../images/' + email ,'dataframe' :'../datadf/' + email ,'SalesPersonName' : fullName}
+    output = template.render(context)
+    # print(output)
+    outHtml = "./thtml/"+email + ".html"
+    with open(outHtml, "+w") as fp:
+        fp.write(output)
+    outPdf = './pdf/' + email+ '.pdf'
+    # pdf.from_file(outHtml, outPdf)
+    dirtoCheck = basePathDestinationFolder + email + "/"
+    if not os.path.exists(dirtoCheck):
+        os.makedirs(dirtoCheck)
+
+    pdf.from_file(outHtml, dirtoCheck + email+'.pdf')
