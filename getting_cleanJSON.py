be6c517be89b134d71dcf8335e96b2a853915c50
Naveen <gsnaveen@yahoo.com>
2019-11-06 10:45:53 -0800

    Create getting_cleanJSON.py

---
+++ getting_cleanJSON.py
@@ -0,0 +1,31 @@
+import json
+
+def processLog(inList):
+    startJSON = False
+    OutData = []
+    theJSONString = ''
+    for row in inList:
+        rowSplit = row.split('\n')
+        for rowSplitVal in rowSplit:
+            if rowSplitVal == '' : continue
+
+            if rowSplitVal.startswith('{'):
+                startJSON = True
+
+            if startJSON and rowSplitVal.endswith('}'):
+                theJSONString += rowSplitVal
+                OutData.append(json.loads(theJSONString))
+                theJSONString = ''
+            else:
+                theJSONString += rowSplitVal
+                
+    return OutData
+
+
+
+inList = ['\n\n\n{"myKey1":[1,2,3,4',
+          '5,6,7,8,',
+          '9,10]}\n{"myKey1":[9,8,7]}']
+
+
+print(processLog(inList))
