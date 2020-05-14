512fb67cff1b1a3060063ae1e9a6598bef85617d
Naveen <gsnaveen@yahoo.com>
2020-05-13 17:48:22 -0700

    Create dataset_timeseries.py

---
+++ dataset_timeseries.py
@@ -0,0 +1,28 @@
+https://machinelearningmastery.com/time-series-forecasting-methods-in-python-cheat-sheet/
+
+# ARIMA example
+from statsmodels.tsa.arima_model import ARIMA
+from random import random
+# contrived dataset
+data = [x + random() for x in range(1, 100)]
+# fit model
+model = ARIMA(data, order=(1, 1, 1))
+model_fit = model.fit(disp=False)
+# make prediction
+yhat = model_fit.predict(len(data), len(data) + 10, typ='levels')
+
+print(yhat)
+
+# SARIMAX example
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from random import random
+# contrived dataset
+data1 = [x + random() for x in range(1, 100)]
+data2 = [x + random() for x in range(101, 200)]
+# fit model
+model = SARIMAX(data1, exog=data2, order=(1, 1, 1), seasonal_order=(0, 0, 0, 0))
+model_fit = model.fit(disp=False)
+# make prediction
+exog2 = [200 + random()]
+yhat = model_fit.predict(len(data1), len(data1), exog=[exog2])
+print(yhat)
