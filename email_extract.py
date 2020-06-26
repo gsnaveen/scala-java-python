3954fbc5cfb0a0572521dfe8536c2cf28e3347e3
Naveen <gsnaveen@yahoo.com>
2020-06-26 13:03:49 -0700

    Update email_extract.py

--- email_extract.py
+++ email_extract.py
@@ -1,21 +1,23 @@
 import re
-
+from collections import defaultdict
 """
 input is a text file output of email
 """
 
 file = 'email1'
-outset = set()
+outset = defaultdict(int)
 with open('./data/email/' + file +'.txt') as f:
     data = f.readlines()
     for row in data:
         for word in row.lower().split(' '):
             if re.search('@',word) and re.search('\.org|\.net|\.com|\..{2}.*',word):
-
-                 outset.add(re.sub('<|>|\'|`|;|,|\n','',word))
+                 # outset.add(re.sub('<|>|\'|`|;|,|\n','',word))
+                email = re.sub('<|>|\'|`|;|,|\n', '', word)
+                outset[email] += 1
 
 
 print('Total email address : ',len(outset))
+print(outset)
 print('+++++++++++++++++++++')
 for email in outset:
     print(email)
