e84e1287e75d7f7e162b3d463e5a58d8c3cadc1a
Naveen <gsnaveen@yahoo.com>
2021-08-31 07:20:47 -0700

    Update tableau_metadata.py

--- tableau_metadata.py
+++ tableau_metadata.py
@@ -5,11 +5,26 @@ import pandas as pd
 baseLoc = "../../data/"
 baseTab = os.path.join(baseLoc, 'tableau')
 baseTabout = os.path.join(baseLoc, 'tableauout')
+
 Details = []
+DetailsKeyword = []
+extension = 'twbx' # 'twbx' 'twb'
+
+def cleanText (inStr):
+
+    base1 = re.sub('\[|\]|\(|\)|\"', '', inStr)
+    base2 = re.sub('_', ' ', base1)
+    base3 = re.sub('\s+', ' ', base2)
+
+    lastStr = base3.lower()
+
+    return lastStr.split(' ')
+
 print(baseTab)
+
+
 for file in os.listdir(baseTab):
-    if file.endswith(".twbx"):
-        # print(file)
+    if file.endswith("."+extension):
         sourceWB = Workbook(os.path.join(baseTab,file))
         worksheets = sourceWB.worksheets
 
@@ -21,21 +36,41 @@ for file in os.listdir(baseTab):
                     for fieldWorksheet in field.worksheets:
 
                         if worksheet == fieldWorksheet:
-                            # print(file,worksheet, "==", fieldWorksheet, "|", count, field.name, "-", field.caption)
-                            Details.append([file,worksheet,datasource.caption, count, field.name, field.caption,field.role])
+                            typeCol = calcSource = ''
+                            if re.search('\(group\)', field.id) : typeCol = 'grouping'
+                            if re.search('Calculation\_', field.id):
+                                typeCol = 'Calculation'
+                                calcSource = field.calculation
+
+                            fieldID = re.sub('\[|\]','',field.id)
+
+                            Details.append([file,worksheet,datasource.caption, count, fieldID, field.name,field.role,typeCol,calcSource,None]) # field.caption
+
+                            for keyword in cleanText(field.name):
+                                DetailsKeyword.append(
+                                    [file, worksheet, datasource.caption, count, fieldID, field.name, field.role,
+                                     typeCol, calcSource, None,keyword])
 
 detailsdc = []
+detailsdcKeyword = []
 for file in os.listdir(baseTab):
-    if file.endswith(".twbx"):
-        # print(file)
+    if file.endswith("."+extension):
         sourceWB = Workbook(os.path.join(baseTab, file))
 
         for ds in sourceWB.datasources:
             for field,value in ds.fields.items():
-                # print(ds.caption,field,value.name,value.caption,value.alias,value.role,value.datatype)
-                detailsdc.append([file,ds.caption,field,value.name,value.caption,value.alias,value.role])
+                detailsdc.append([file,ds.caption,re.sub('\[|\]','',field),value.name,value.caption,value.alias,value.role])
+                for keyword in cleanText(value.name):
+                    detailsdcKeyword.append(
+                        [file, ds.caption, re.sub('\[|\]', '', field), value.name, value.caption, value.alias,
+                         value.role,keyword])
 
-df = pd.DataFrame.from_records(Details,columns=['workbook','worksheet','connection','fieldnumber','name','label','type'])
-df.to_csv(os.path.join(baseTabout, 'wbfields.tsv'),header=True,index=False,sep='\t')
+df = pd.DataFrame.from_records(Details,columns=['workbook','worksheet','connection','fieldnumber','name','label','type','grpCalc','calcSource','basetable'])
+df.to_csv(os.path.join(baseTabout, extension + '_wbfields.tsv'),header=True,index=False,sep='\t')
+df = pd.DataFrame.from_records(DetailsKeyword,columns=['workbook','worksheet','connection','fieldnumber','name','label','type','grpCalc','calcSource','basetable','keyword'])
+df.to_csv(os.path.join(baseTabout, extension + '_wbfields_keyword.tsv'),header=True,index=False,sep='\t')
+
 df = pd.DataFrame.from_records(detailsdc,columns=['workbook','connection','fieldname','name','caption','alias','role'])
-df.to_csv(os.path.join(baseTabout, 'wbfieldscon.tsv'),header=True,index=False,sep='\t')
+df.to_csv(os.path.join(baseTabout, extension + '_wbfieldscon.tsv'),header=True,index=False,sep='\t')
+df = pd.DataFrame.from_records(detailsdcKeyword,columns=['workbook','connection','fieldname','name','caption','alias','role','keyword'])
+df.to_csv(os.path.join(baseTabout, extension + '_wbfieldscon_keyword.tsv'),header=True,index=False,sep='\t')
