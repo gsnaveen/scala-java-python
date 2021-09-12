61649cada53ee1a18e6836b3eb0825650839a313
Naveen <gsnaveen@yahoo.com>
2021-09-11 20:01:04 -0700

    Update postgres_csvInsert.py

--- postgres_csvInsert.py
+++ postgres_csvInsert.py
@@ -3,9 +3,22 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 DATA = './data/'
+"""
 alchemyEngine = create_engine('postgresql+psycopg2://{userid}:{pword}@{host}/{db}'.format(userid=userid, pword=passWord,
                                                                                       db=database,port=port,host=host), pool_recycle=3600);
 postgreSQLConnection = alchemyEngine.connect();
+"""
+
+try:
+    conn = psycopg2.connect(
+        "dbname={db} user={userid} host={host} port={port} password={pword}".format(userid=userid, pword=passWord,
+                                                                                    db=database, port=port,
+                                                                                    host=host))
+except Exception as ex:
+    print("I am unable to connect to the database" + str(ex))
+
+cur = conn.cursor()
+    
 
 file = 'csv'
 conn = postgreSQLConnection
@@ -16,22 +29,25 @@ tableSchemaSQLbase = '''select column_name,data_type from information_schema.col
 schemaName = 'metadata'
 folder = 'metadata_file'
 fileName = 'loadData'
-tableName = 'metadata_file'.format(schema=schemaName)
+tableName = 'metadata_file'
 
 tableSchemaSQL = tableSchemaSQLbase.format(table_schema=schemaName, table_name=tableName)
 
-tabelDeffdata = conn.execute(tableSchemaSQL)
-tabelDeffList = tabelDeffdata.fetchall()
+cur.execute(tableSchemaSQL)
+tabelDeffList = cur.fetchall()
 
 tabDeff = {}
 for row in tabelDeffList:
     tabDeff[row[0]] = row[1]
 
-infile = os.path.join(DATA, 'toDB', folder, fileName + '.tsv')
+infile = os.path.join(DATA, 'ps', 'toDB', folder, fileName + '.tsv')
 
 df = pd.read_csv(infile, header=0, sep='\t')
+df.columns = [col.lower() for col in df.columns ]
 dataCols = set(df.columns)
+print(dataCols)
 numRows = df.shape[0]
+
 dataColsInDB = [col for col in dataCols if col in tabDeff]
 dataColsInDBNot = [col for col in dataCols if col not in tabDeff]
 
@@ -49,27 +65,53 @@ insertSQLHeader = '''
                         {schema}.{table} ({toInsertAttr})
                     VALUES
                     '''.format(schema=schemaName, table=tableName, toInsertAttr=toInsertAttr)
-
 valuesStr = ''
+
 for index, row in df.iterrows():
     valuesStr += '('
+    valuesStr1 = '('
     for col in tabDiffInData:
 
+        theVal = "{val}".format(val=row[col]).strip()
+        if (theVal == 'nan') or (pd.isna(theVal)) or (pd.isnull(theVal)) or (theVal == None) or (theVal == ''):
+            theVal = 'null'
+
         if tabDeff[col] in ['integer', 'bigint', 'double precision', 'numeric']:
-            valuesStr += "{val},".format(val=row[col])
+            valuesStr += "{val},".format(val=theVal)
+            valuesStr1 += "{val},".format(val=theVal)
+
+        elif theVal == 'null':
+            valuesStr += "{val},".format(val=theVal)
+            valuesStr1 += "{val},".format(val=theVal)
         else:
-            valuesStr += "'{val}',".format(val=row[col])
+            valuesStr += "'{val}',".format(val=theVal)
+            valuesStr1 += "'{val}',".format(val=theVal)
 
     else:
+        valuesStr1 += 'current_timestamp);'
         if numRows == index + 1:
             valuesStr += 'current_timestamp); \n'
         else:
             valuesStr += 'current_timestamp), \n'
 
-insertStmt = insertSQLHeader + valuesStr
+    if insert1 :
+        try:
+            cur.execute(insertSQLHeader + valuesStr1)
+            conn.commit()
+        except:
+            insertErrors.append(index)
+            print(index,insertSQLHeader + valuesStr1)
 
-try:
-    conn.execute(insertStmt)
-except:
-    raise
-    print("Insert error")
+
+if not insert1 :
+    try:
+        insertStmt = insertSQLHeader + valuesStr
+        print(insertStmt)
+        cur.execute(insertStmt)
+        conn.commit()
+    except:
+        print("Insert error")
+        raise
+
+cur.close()
+conn.close()
