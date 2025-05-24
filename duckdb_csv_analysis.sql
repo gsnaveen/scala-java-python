c9986090223de86921f405e84f45aa8e21abc6d0
Naveen <8109420+gsnaveen@users.noreply.github.com>
2025-05-23 20:25:07 -0700

    Create duckdb_csv_analysis.sql

---
+++ duckdb_csv_analysis.sql
@@ -0,0 +1,11 @@
+CREATE TABLE mytable AS
+    SELECT * FROM read_csv('mydata.csv');
+
+
+CREATE TABLE mytable_summary AS SELECT * FROM (SUMMARIZE mytable);
+
+COPY (SELECT * FROM mytable_summary) TO 'mytable_summary.csv' (HEADER, DELIMITER ',');
+
+CREATE TABLE mytable_varchar_len as
+Select MAX(LEN("Attribute Name"))
+from mytable
