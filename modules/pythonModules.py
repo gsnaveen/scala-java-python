f813c68d1a4561f9b15e20612096e98e18cad0d5
Naveen <8109420+gsnaveen@users.noreply.github.com>
2025-10-30 17:35:10 -0700

    Create pythonModules.py

---
+++ modules/pythonModules.py
@@ -0,0 +1,8 @@
+
+pip install tinydb
+pip install filexdb -- similar to Mongo
+pip install elara   -- similar to redis
+
+pip install chromadb -- RAG Applications
+pip install duckdb   -- analytics database
+
