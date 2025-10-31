317b0cf493c131659b0fe09839062430797a82f3
Naveen <8109420+gsnaveen@users.noreply.github.com>
2025-10-30 18:14:43 -0700

    Update pythonModules.py

--- modules/pythonModules.py
+++ modules/pythonModules.py
@@ -6,6 +6,9 @@ pip install elara   -- similar to redis
 pip install chromadb -- RAG Applications
 pip install duckdb   -- analytics database
 pip install cogdb    -- Graph database
+pip install node2vec
+pip install deepwalk
+
 
 --Network
 pip install networkx[default,extra]
